6c07966ec33bfc4ad3bfdbb1f6d403c3cf1b7617
Kevin Bluer <kevin@bluer.com>
2022-04-22 12:45:40 -0500

    fixing script

--- scripts/debugExtension.js
+++ scripts/debugExtension.js
@@ -1,11 +1,17 @@
-module.exports = async (callback) => {
-  // TODO: implement your actions
-  let ss = await SimpleStorage.deployed();
-  let multisig = await MultiSignatureWallet.deployed();
-  let accounts = await web3.eth.getAccounts();
-  let encoded = '0x60fe47b10000000000000000000000000000000000000000000000000000000000000005';
-  multisig.submitTransaction(ss.address, 0, encoded, {from: accounts[0]})
+const SimpleStorage = artifacts.require('SimpleStorage');
+const MultiSignatureWallet = artifacts.require('MultiSignatureWallet');
 
-  // invoke callback
+module.exports = async (callback) => {
+  try {
+    const ss = await SimpleStorage.deployed();
+    const multisig = await MultiSignatureWallet.deployed();
+    const accounts = await web3.eth.getAccounts();
+    const encoded = '0x60fe47b10000000000000000000000000000000000000000000000000000000000000005';
+    const result = await multisig.submitTransaction(ss.address, 0, encoded, {from: accounts[0]})
+    console.log(result);
+
+  } catch(err) {
+    console.log('doooh', err.message);
+  }
   callback();
 };
